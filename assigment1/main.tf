3f1474018b551a82159217d54bff9b2d5cced29e
sly01 <aerkoc01@gmail.com>
2019-07-02 15:40:35 +0200

    test var and output added

--- assigment1/main.tf
+++ assigment1/main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "instance_flavor" {
+  default     = "t2.micro"
+  description = "Size of instance"
+}
+
 data "aws_ami" "latest-amazon-linux2" {
   most_recent = true
   owners      = ["137112412989"] # Amazon
@@ -22,16 +27,17 @@ data "aws_ami" "latest-amazon-linux2" {
   }
 }
 
-#output "ami_id" {
-#  value = "${data.aws_ami.latest-amazon-linux2.id}"
-#}
-
 resource "aws_instance" "server1" {
   #ami = "ami-0c6b1d09930fac512"
-  ami           = "${data.aws_ami.latest-amazon-linux2.id}"
-  instance_type = "t2.micro"
+  ami = "${data.aws_ami.latest-amazon-linux2.id}"
+  #instance_type = "t2.micro"
+  instance_type = "${var.instance_flavor}"
 
   tags = {
     Name = "server1"
   }
+}
+
+output "server1_public_hostname" {
+  value = "${aws_instance.server1.public_dns}"
 }
